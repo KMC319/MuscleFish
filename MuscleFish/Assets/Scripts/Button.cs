1e4fa1d3b83838f95928ff70ca266d2ea4c8a772
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2019-08-07 02:13:11 +0900

    add

---
+++ MuscleFish/Assets/Scripts/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void onclick()
+    {
+        Debug.Log("Button click");
+        //非表示にする
+        gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
